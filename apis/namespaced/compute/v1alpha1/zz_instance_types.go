a6622705dc822f8df538239a42b0b72381ee56d7
Albertone Quentin <quentin.albertone@exoscale.ch>
2026-08-31 17:51:33 +0200

    feat: bump exoscale terraform provider

--- apis/namespaced/compute/v1alpha1/zz_instance_types.go
+++ apis/namespaced/compute/v1alpha1/zz_instance_types.go
@@ -79,7 +79,7 @@ type InstanceInitParameters struct {
 	EnableTpm *bool `json:"enableTpm,omitempty" tf:"enable_tpm,omitempty"`
 
 	// (Boolean) Enable IPv6 on the instance (boolean; default: false).
-	// Enable IPv6 on the instance (boolean; default: `false`).
+	// Enable IPv6 on the instance (boolean; default: `false`). Can not be disabled after being enabled.
 	IPv6 *bool `json:"ipv6,omitempty" tf:"ipv6,omitempty"`
 
 	// (Map of String) A map of key/value labels.
@@ -136,7 +136,7 @@ type InstanceInitParameters struct {
 	SecurityGroupIdsSelector *v1.NamespacedSelector `json:"securityGroupIdsSelector,omitempty" tf:"-"`
 
 	// (String) The instance state (running or stopped; default: running).
-	// The instance state (`running` or `stopped`; default: `running`).
+	// The instance state (`running` or `stopped`). If omitted, instance will start and reach `running` state.
 	State *string `json:"state,omitempty" tf:"state,omitempty"`
 
 	// (String) ❗ The exoscale_template (ID) to use when creating the instance.
@@ -201,7 +201,7 @@ type InstanceObservation struct {
 	ID *string `json:"id,omitempty" tf:"id,omitempty"`
 
 	// (Boolean) Enable IPv6 on the instance (boolean; default: false).
-	// Enable IPv6 on the instance (boolean; default: `false`).
+	// Enable IPv6 on the instance (boolean; default: `false`). Can not be disabled after being enabled.
 	IPv6 *bool `json:"ipv6,omitempty" tf:"ipv6,omitempty"`
 
 	// (String) The instance (main network interface) IPv6 address (if enabled).
@@ -257,7 +257,7 @@ type InstanceObservation struct {
 	SecurityGroupIds []*string `json:"securityGroupIds,omitempty" tf:"security_group_ids,omitempty"`
 
 	// (String) The instance state (running or stopped; default: running).
-	// The instance state (`running` or `stopped`; default: `running`).
+	// The instance state (`running` or `stopped`). If omitted, instance will start and reach `running` state.
 	State *string `json:"state,omitempty" tf:"state,omitempty"`
 
 	// (String) ❗ The exoscale_template (ID) to use when creating the instance.
@@ -350,7 +350,7 @@ type InstanceParameters struct {
 	EnableTpm *bool `json:"enableTpm,omitempty" tf:"enable_tpm,omitempty"`
 
 	// (Boolean) Enable IPv6 on the instance (boolean; default: false).
-	// Enable IPv6 on the instance (boolean; default: `false`).
+	// Enable IPv6 on the instance (boolean; default: `false`). Can not be disabled after being enabled.
 	// +kubebuilder:validation:Optional
 	IPv6 *bool `json:"ipv6,omitempty" tf:"ipv6,omitempty"`
 
@@ -416,7 +416,7 @@ type InstanceParameters struct {
 	SecurityGroupIdsSelector *v1.NamespacedSelector `json:"securityGroupIdsSelector,omitempty" tf:"-"`
 
 	// (String) The instance state (running or stopped; default: running).
-	// The instance state (`running` or `stopped`; default: `running`).
+	// The instance state (`running` or `stopped`). If omitted, instance will start and reach `running` state.
 	// +kubebuilder:validation:Optional
 	State *string `json:"state,omitempty" tf:"state,omitempty"`
 
